2e57664c3a57df6df99c7d3feb9bb35b3efb34fb
zhouyunjin <15989558059@163.com>
2017-02-23 01:32:37 +0800

    增加一些注释

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -8,13 +8,18 @@ use common\models\PostSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
 use common\models\Tag;
 use common\models\Comment;
+use common\models\User;
+use yii\rest\Serializer;
 /**
  * PostController implements the CRUD actions for Post model.
  */
 class PostController extends Controller
 {
+    public $added=0; //0代表还没有新回复
     /**
      * @inheritdoc
      */
@@ -125,5 +130,46 @@ class PostController extends Controller
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
+    }
+    public function actionDetail($id)
+    {
+        //step1. 准备数据模型
+        $model = $this->findModel($id);
+        $tags=Tag::findTagWeights();
+        $recentComments=Comment::findRecentComments();       
+        $commentModel = new Comment();
+         
+        //step2. 当评论提交时，处理评论
+        
+        if($commentModel->load(Yii::$app->request->post()))
+        {
+            if(!Yii::$app->user->isGuest)
+            {
+                $userMe = User::findOne(Yii::$app->user->id);
+                $commentModel->email = $userMe->email;
+                $commentModel->userid = $userMe->id;
+                
+                $commentModel->status = 1; //新评论默认状态为 pending
+                $commentModel->post_id = $id;
+                if($commentModel->save())
+                {
+                    $this->added=1;
+                }
+            }
+          else {
+              return $this->redirect('?r=site/login');
+          }
+        }
+         
+        //step3.传数据给视图渲染
+         
+        return $this->render('detail',[
+            'model'=>$model,
+            'tags'=>$tags,
+            'recentComments'=>$recentComments,
+            'commentModel'=>$commentModel,
+            'added'=>$this->added,
+        ]);
+         
     }
 }
